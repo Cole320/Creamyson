4e1c8224f58770fc19034bba7fa3f7872a8ae662
Cole320 <colebrazinski15@gmail.com>
2021-05-08 23:58:48 -0400

    This is the beginning of a very good thing

---
+++ src/fpsgame/creamy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by coal on 5/8/21.
+//
+
+#include "creamy.h"
---
+++ src/fpsgame/creamy.h
@@ -0,0 +1,14 @@
+//
+// Created by coal on 5/8/21.
+//
+
+#ifndef CREAMYSON_CREAMY_H
+#define CREAMYSON_CREAMY_H
+
+
+class creamy {
+
+};
+
+
+#endif //CREAMYSON_CREAMY_H
